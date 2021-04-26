911d3ae52f8e067b1f1f3e121ef9ab388481f092
reyalvarado <reyalvarado@yahoo.com>
2021-04-26 02:34:03 +0000

    Lab 2 part 1 Concept2

--- main.js
+++ main.js
@@ -38,18 +38,15 @@ function getFirstIpAddress(cidrStr, callback) {
     // Notice the destructering assignment syntax to get the value of the first array's element.
     [firstIpAddress] = cidr.toArray(options);
     // getIpv4MappedIpv6Address (firstIpAddress);
+    // need to concate string result to firstIpAddress here - 
+    // firstIpAddress=firstIpAddress+ipv6Address;
   }
-
-  // need to concate string result to firstIpAddress here
-
-  // firstIpAddress=firstIpAddress+ipv6Address;
 
   // Call the passed callback function.
   // Node.js convention is to pass error data as the first argument to a callback.
   // The IAP convention is to pass returned data as the first argument and error
   // data as the second argument to the callback function.
-
-
+  
   return callback(firstIpAddress, callbackError);
 }
 
